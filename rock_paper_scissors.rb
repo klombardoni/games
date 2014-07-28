b29e6ec54b0a31565c9052a303cf764396627ba2
Sharon <sharon@Sharons-MacBook-Air.local>
2014-07-28 18:29:40 -0400

    first commit

---
+++ rock_paper_scissors.rb
@@ -0,0 +1,25 @@
+def play_game()
+	
+	choices = ["rock", "paper", "scissors"]
+	computer_choice = choices.sample
+
+	puts "Rock, paper, scissors, shoot!"
+
+	human_choice = gets.chomp
+
+	puts "You chose #{human_choice}, I chose #{computer_choice}."
+
+	if human_choice == computer_choice
+   		puts "It's a tie!"
+
+		elsif   
+      		  human_choice == "scissors" && computer_choice == "rock" ||
+	   	      human_choice == "rock" && computer_choice == "paper" ||
+	   	      human_choice == "paper" && computer_choice == "scissors"
+	     	  puts "Computer wins!"
+	    else 
+		      puts "Human wins!"
+	end
+end
+
+play_game()
